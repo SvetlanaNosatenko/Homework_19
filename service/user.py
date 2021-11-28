712fe0823332097544cf2cf8e01dfe5760fd8155
Svetlana Nosatenko <s.v.nosatenko@gmail.com>
2021-11-28 23:28:54 +0300

    first commit

---
+++ service/user.py
@@ -0,0 +1,32 @@
+import hashlib
+from dao.user import UserDAO
+from constants import PWD_HASH_SALT, PWD_HASH_ITERATIONS
+
+
+class UserService:
+    def __init__(self, dao: UserDAO):
+        self.dao = dao
+
+    def get_one(self, bid):
+        return self.dao.get_one(bid)
+
+    def get_all(self):
+        return self.dao.get_all()
+
+    def create(self, user_d):
+        return self.dao.create(user_d)
+
+    def update(self, user_d):
+        self.dao.update(user_d)
+        return self.dao
+
+    def get_hash(password):
+        return hashlib.pbkdf2_hmac(
+            'sha256',
+            password.encode('utf-8'),  # Convert the password to bytes
+            PWD_HASH_SALT,
+            PWD_HASH_ITERATIONS
+        ).decode("utf-8", "ignore")
+
+
+
