afba1dc6271fbe11d347c560b3cb4adf686329d5
Svetlana Nosatenko <s.v.nosatenko@gmail.com>
2021-12-13 02:13:04 +0300

    add auth

---
+++ auth.py
@@ -0,0 +1,105 @@
+import calendar
+import datetime
+import hashlib
+
+from flask_restx import Resource, Namespace
+from flask import abort, request
+import jwt
+from constants import *
+from dao.model.user import User
+from setup_db import db
+
+auth_ns = Namespace('auth')
+
+
+def auth_required(func):
+    def wrapper(*args, **kwargs):
+        if "Authorization" not in request.headers:
+            abort(401)
+        data = request.headers['Authorization']
+        token = data.split("Bearer ")[-1]
+        try:
+            jwt.decode(token, secret, algorithms=algo)
+        except Exception as e:
+            print("JWT Decode Exception", e)
+            abort(401)
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def admin_required(func):
+    def wrapper(*args, **kwargs):
+        if "Authorization" not in request.headers:
+            abort(401)
+        data = request.headers['Authorization']
+        token = data.split("Bearer ")[-1]
+        try:
+            user = jwt.decode(token, secret, algorithms=algo)
+            role = user.get("role")
+            if role != "admin":
+                abort(401)
+        except Exception as e:
+            print("JWT Decode Exception", e)
+            abort(401)
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+@auth_ns.route('/')
+class AuthView(Resource):
+    def post(self):
+        req_json = request.json
+        user = req_json.get('username')
+        password = req_json.get('password')
+
+        if None in [user, password]:
+            abort(400)
+
+        user = db.session.query(User).filter(User.username == user).all()
+        if user is None:
+            return {"error": "Неверные учётные данные"}, 401
+
+        # password_hash = hashlib.md5(password.encode('utf-8')).hexdigest()
+        password_hash = hashlib.pbkdf2_hmac(algo, password.encode(), PWD_HASH_SALT, PWD_HASH_ITERATIONS)
+
+        if password_hash != user.password:
+            return {"error": "Неверные учётные данные"}, 401
+
+        data = {
+            "username": user.get('username'),
+            "password": password.get('password')
+        }
+        min30 = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+        data["exp"] = calendar.timegm(min30.timetuple())
+        access_token = jwt.encode(data, secret, algorithm=algo)
+        days130 = datetime.datetime.utcnow() + datetime.timedelta(days=130)
+        data["exp"] = calendar.timegm(days130.timetuple())
+        refresh_token = jwt.encode(data, secret, algorithm=algo)
+
+        return {"access_token": access_token, "refresh_token": refresh_token}, 201
+
+    def put(self):
+        req_json = request.json
+        refresh_token = req_json.get("refresh_token")
+        if refresh_token is None:
+            abort(400)
+
+        data = jwt.decode(jwt=refresh_token, key=secret, algorithms=[algo])
+
+        username = data.get("username")
+        user = db.session.query(User).filter(User.username == username).first()
+
+        data = {"username": user.username,
+                "password": user.password,
+                "role": user.role
+                }
+        min30 = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+        data["exp"] = calendar.timegm(min30.timetuple())
+        access_token = jwt.encode(data, secret, algorithm=algo)
+        days130 = datetime.datetime.utcnow() + datetime.timedelta(days=130)
+        data["exp"] = calendar.timegm(days130.timetuple())
+        refresh_token = jwt.encode(data, secret, algorithm=algo)
+
+        return {"access_token": access_token, "refresh_token": refresh_token}, 201
--- views/movies.py
+++ views/movies.py
@@ -1,6 +1,7 @@
 from flask import request
 from flask_restx import Resource, Namespace
 
+from auth import auth_required, admin_required
 from dao.model.movie import MovieSchema
 from implemented import movie_service
 
@@ -9,6 +10,7 @@ movie_ns = Namespace('movies')
 
 @movie_ns.route('/')
 class MoviesView(Resource):
+    @auth_required
     def get(self):
         director = request.args.get("director_id")
         genre = request.args.get("genre_id")
@@ -22,6 +24,8 @@ class MoviesView(Resource):
         res = MovieSchema(many=True).dump(all_movies)
         return res, 200
 
+    @admin_required
+    @auth_required
     def post(self):
         req_json = request.json
         new_movie = movie_service.create(req_json)
@@ -30,11 +34,14 @@ class MoviesView(Resource):
 
 @movie_ns.route('/<int:bid>')
 class MovieView(Resource):
+    @auth_required
     def get(self, bid):
         b = movie_service.get_one(bid)
         sm_d = MovieSchema().dump(b)
         return sm_d, 200
 
+    @admin_required
+    @auth_required
     def put(self, bid):
         req_json = request.json
         if "id" not in req_json:
@@ -42,6 +49,8 @@ class MovieView(Resource):
         movie_service.update(req_json)
         return "", 204
 
+    @admin_required
+    @auth_required
     def delete(self, bid):
         movie_service.delete(bid)
         return "", 204
